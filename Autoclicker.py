0ac049018d5d4d9491c7c0309f1a3f4eefa15cad
thom0075 <68689661+thom0075@users.noreply.github.com>
2021-04-18 10:23:14 -0700

    Update Autoclicker.py

--- Autoclicker.py
+++ Autoclicker.py
@@ -1,4 +1,8 @@
-import pyautogui
-while True:
-    pyautogui.click()
-    
+import pyautogui    #imports module
+while True: #creates infinite loop
+    pyautogui.click()   #calls the function
+"""
+To stop the program, drag the mouse to one of the four 
+corners of your monitor, it will generate a FailSafeException that
+will immediately stop the program execution
+"""
